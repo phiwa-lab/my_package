d55acaf0037586a5db270b002c0728e3b76f7386
phiwa-lab <60380888+phiwa-lab@users.noreply.github.com>
2020-02-07 13:28:14 +0200

    fixed_ommitted_stuff

---
+++ my_package/myModule.py
@@ -0,0 +1,16 @@
+def top_n(items, n):
+    """
+    docstring goes here
+    """
+
+    for i in range(n):  # keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1]:  # if this item is bigger than next item..
+                items[j], items[j+1] = items[j+1], items[j]  # swap the two!
+
+    # get last two items
+    top_n = items[-n:]
+
+    # return in descending order
+    return top_n[::-1]
